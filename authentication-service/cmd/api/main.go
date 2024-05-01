c05c3bd8607571cbd46cfba8e5c39d69c410fca0
Dao Pham <daopm.dean@gmail.com>
2024-05-02 00:32:35 +0700

    auth service

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = "80"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: app.routes(),
+	}
+
+	log.Printf("Authentication service started on %s\n", port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Println(err)
+	}
+}
--- authentication-service/go.mod
+++ authentication-service/go.mod
@@ -1,3 +1,9 @@
 module authentication
 
 go 1.19
+
+require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/cors v1.2.1
+	golang.org/x/crypto v0.22.0
+)
