fee964731365f899c4071c99a2d900b3a9f7ce91
Dao Pham <daopm.dean@gmail.com>
2024-05-02 15:34:36 +0700

    update authen service

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -6,9 +6,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const port = "80"
+
+var counts = 0
 
 type Config struct {
 	DB     *sql.DB
@@ -16,7 +24,15 @@ type Config struct {
 }
 
 func main() {
-	app := Config{}
+	conn := connectDB()
+	if conn == nil {
+		log.Panic("can not connect to postgres!")
+	}
+
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", port),
@@ -28,4 +44,40 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func connectDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		db, err := openDB(dsn)
+		if err != nil {
+			log.Println("postgres not yet ready...", err)
+			counts++
+		} else {
+			return db
+		}
+
+		if counts > 10 {
+			log.Println("counts > 10, too many fails")
+			return nil
+		}
+
+		log.Println("retry connecting postgres in 2 seconds...")
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
